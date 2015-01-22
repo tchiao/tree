6b771a3f29f28554ffff98c1f865b284075faeba
tchiao <tiffanya.chiao@gmail.com>
2015-01-21 21:55:54 -0800

    Moved Javascript loading, slight style change to loading gif

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = ENV['ASSET_HOST']
+  config.font_assets.origin = 'http://getbootstrap.com/'
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
